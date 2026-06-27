e0c97fbeea281d906c86974ae3ca9b60e9eb3414
Sonal-1610 <152240291+Sonal-1610@users.noreply.github.com>
2026-06-27 15:56:06 +0530

    Time: 30 ms (32.88%), Space: 139.3 MB (42.43%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+        vector<int> ans;
+        int i = 0, j = 0;
+        while (j < nums.size()) {
+            while (!q.empty() && q.back() < nums[j]) {
+                q.pop_back();
+            }
+            q.push_back(nums[j]);
+            if (j - i + 1 == k) {
+                int ele = q.front();
+                ans.push_back(ele);
+                if (nums[i] == q.front()) {
+                    q.pop_front();
+                }
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
