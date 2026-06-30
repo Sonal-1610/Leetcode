2b235a9798dddfb7e105529fd8728aec9ce21ea7
Sonal-1610 <152240291+Sonal-1610@users.noreply.github.com>
2026-06-30 16:23:34 +0530

    Time: 6 ms (22.88%), Space: 65 MB (96.34%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& arr, int k) {
+      
+        // code here
+        if (k <= 1) return 0;
+        int i=0,j=0;
+        long long prod=1;
+        int count=0;
+        while(j<arr.size()){
+            prod=prod*arr[j];
+            while(prod>=k){
+                prod=prod/arr[i];
+                i++;
+            }
+            count+=j-i+1;
+            j++;
+        }
+        return count;
+    
+    }
+};
